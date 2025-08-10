d42fa1ede9bdc230e091ff726152d2e2c6a692ff
neilt <neiltran.syd@gmail.com>
2025-08-10 11:42:21 +1000

    fix background

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [highScore, setHighScore] = useState(0);
 
   return (
-    <div className="bg-cover bg-center bg-[url(./src/assets/background1.png)] min-h-screen flex flex-col">
+    <div className="bg-cover bg-center bg-[url('/background1.png')] min-h-screen flex flex-col">
       <div className="flex-shrink-0 py-4">
         <ScoreBoard score={score} highScore={highScore} />
       </div>
