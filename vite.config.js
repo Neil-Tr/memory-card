c0b84714bfe51cd5ef51663fc4522fd0890e019e
neilt <neiltran.syd@gmail.com>
2025-08-11 21:11:35 +1000

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/memory-card/",
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/memory-card/" // for GitHub Pages
+      : "/", // for Netlify & local dev
 });
