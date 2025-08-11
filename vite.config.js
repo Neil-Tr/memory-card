0696f80d1b59d313411e0f144c43b7b34176938b
neilt <neiltran.syd@gmail.com>
2025-08-11 21:57:33 +1000

    update config to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:
-    process.env.NODE_ENV === "production" &&
-    process.env.DEPLOY_TARGET === "gh-pages"
-      ? "/memory-card/" // for GitHub Pages
-      : "/", // for Netlify & local dev
+  base: isGitHubPages ? "/memory-card/" : "/",
 });
